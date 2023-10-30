bd570eb4534245886c1a174313b8c03d39596b29
Panagiotis Chatzigiannis <axpanos@yahoo.gr>
2023-10-30 15:41:25 +0200

    Created taskApi

--- lib/redux/api/taskApi.ts
+++ lib/redux/api/taskApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Task } from '@/lib/models/taskModel'
 
-export const taskApi = createApi({
+const taskApi = createApi({
   // baseUrl: 'http://192.168.1.135:5050/api'
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5050/api' }),
   endpoints: (builder) => ({
